45fd60e24f181af3ce21a001640b621502116691
Luis Gustavo <piaizete123@gmail.com>
2023-06-21 11:08:50 -0300

    merging

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
-#Reddit
-client_id='client id'
-client_secret='client secret'
-password='reddit password'
-user_agent='Any title for your script'
-username='reddit username'
+# Reddit
+subreddit_name = 'subreddit name'
+client_id = 'client id'
+client_secret = 'client secret'
+user_agent = 'Any title for your script'
+username = 'reddit username'
+password = 'reddit password'
 
-#Twitter
-api_key='api key'
-api_key_secret='api key secret'
-bearer_token='bearer token'
-access_token='access token'
-access_token_secret='access token secret'
+# Post
+hashtags: list = []  # hashtags to use
+
+# Twitter
+api_key = 'api key'
+api_key_secret = 'api key secret'
+bearer_token = 'bearer token'
+access_token = 'access token'
+access_token_secret = 'access token secret'
