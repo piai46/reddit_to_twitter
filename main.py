3dbfd0709583ad26efa3e7096a8c3646025ce678
Luis Gustavo <piaizete123@gmail.com>
2023-06-20 23:52:23 -0300

    Using api v2 to make tweet and api v1.1 to upload media

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from time import sleep
-import config, requests, tweepy, praw
 from datetime import datetime
+from time import sleep
+
+import praw
+import requests
+import tweepy
+
+import config
 
 
 class ProjectZomboid:
@@ -24,7 +29,7 @@ class ProjectZomboid:
             print('Login failed')
             print(f'Error: {error}')
             print(50*"-")
-    
+
     def download_image(self, url):
         try:
             print('Downloading image...')
@@ -39,22 +44,31 @@ class ProjectZomboid:
     def login_twitter(self):
         try:
             print('Logging twitter...')
+            client = tweepy.Client(
+                consumer_key=config.api_key,
+                consumer_secret=config.api_key_secret,
+                access_token=config.access_token,
+                access_token_secret=config.access_token_secret
+            )
             auth = tweepy.OAuthHandler(config.api_key, config.api_key_secret)
-            auth.set_access_token(config.access_token, config.access_token_secret)
+            auth.set_access_token(config.access_token,
+                                  config.access_token_secret)
             api = tweepy.API(auth)
             print('Login succesful')
             print(50*"-")
-            return api
+            return api, client
         except Exception as error:
             print('Login failed')
             print(f'Error: {error}')
             print(50*"-")
 
-    def make_tweet(self, api, content):
+    def make_tweet(self, api: tweepy.API, client: tweepy.Client, content):
         try:
             print('Posting tweet...')
             image_path = 'image.jpg'
-            api.update_with_media(image_path, content)
+            media = api.media_upload(image_path)
+            media_id = media.media_id_string
+            client.create_tweet(text=content, media_ids=[media_id])
             print('Tweet posted!')
         except Exception as error:
             print(f'Failed to post, error: {error}')
@@ -80,19 +94,21 @@ class ProjectZomboid:
 
     def main(self):
         reddit = self.login_reddit()
-        api = self.login_twitter()
+        api, client = self.login_twitter()
         while True:
             try:
-                for submission in reddit.subreddit("projectzomboid").hot(limit=10):
+                for submission in reddit.subreddit(config.subreddit_name).hot(limit=10):
                     print('Fetching submission from reddit')
                     check_id = self.check_id(submission.id)
-                    if check_id == False:
-                        if submission.stickied == False:
+                    if check_id is False:
+                        if submission.stickied is False:
                             url = submission.url
-                            title = f'{submission.title} redd.it/{str(submission)}\n#ProjectZomboid'
+                            title = f'{submission.title} redd.it/{str(submission)}\n'
+                            for hashtag in config.hashtags:
+                                title += f'#{hashtag} '
                             if 'jpg' in url or 'png' in url:
                                 self.download_image(url)
-                                self.make_tweet(api, title)
+                                self.make_tweet(api, client, title)
                                 self.add_id(submission.id)
                                 print(50*"-")
                                 sleep(300)
@@ -103,13 +119,16 @@ class ProjectZomboid:
                             print('Submission stickied, skipping...')
                             print(50*"-")
             except Exception as error:
-                print(f'Error while taking informations from reddit. Error: {error}')
+                print(
+                    f'Error while taking informations from reddit. Error: {error}')
                 sleep(60)
             now_date = datetime.now()
-            print(f"{now_date.strftime('%d')}/{now_date.strftime('%m')}, {now_date.hour}:{now_date.minute} - ", end="")
+            print(
+                f"{now_date.strftime('%d')}/{now_date.strftime('%m')}, {now_date.hour}:{now_date.minute} - ", end="")
             print('End of submissions, sleeping for 1 hour')
             print(50*"-")
             sleep(3600)
+
 
 if __name__ == '__main__':
     pz = ProjectZomboid()
